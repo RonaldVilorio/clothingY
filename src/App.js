8b1e75b217d4be6683238e03f67b4a64c0ea5ccc
RonaldVilorio <ronvilorio@gmail.com>
2022-06-10 10:54:57 -0400

    mock shop data, shop comp added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import {Routes,Route} from "react-router-dom"
 import Home from "./routes/home/home.component"
 import Navigation from "./routes/navigation/navigation.component"
 import SignIn from "./routes/sign-in-page/sign-in-page.component"
-const Shop = () =>{
-  return <h1>Hey there</h1>
-}
+import Shop from "./routes/shop/shop.component"
 
 const App = ()=>{
   return (
