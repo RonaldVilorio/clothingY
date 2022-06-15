7443c09302fb292c632dd8533b4c6c6252ba7e52
RonaldVilorio <ronvilorio@gmail.com>
2022-06-15 13:08:21 -0400

    styled component transfer for navigation comp

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,42 +1,49 @@
 import {Fragment, useContext} from "react"
-import {Outlet,Link} from "react-router-dom"
+import {Outlet} from "react-router-dom"
 import {ReactComponent as YLogo} from "../../assets/crown.svg"
-import "./navigation.styles.scss"
 import { UserContext } from "../../contexts/user.context"
 import { CartDropDownContext } from "../../contexts/cart-drop-down.context"
 import CartIcon from "../../components/cart-icon/cart-icon.component"
 import CartDropDown from "../../components/cart-dropdown/cart-dropdown.component"
+import {
+    NavigationContainer,
+    LogoContainer,
+    NavLinksContainer,
+    NavLink
+} from "./navigation.styles"
+import { signOutUser } from "../../utils/firebase/firebase.utils"
+
 const Navigation = ()=>{
     const {currentUser} = useContext(UserContext)
     const {cartDropDown} = useContext(CartDropDownContext)
 
     return(
         <Fragment>
-            <div className="navigation">
-                <Link className="logo-container" to="/">
-                    <YLogo className="logo"/>
-                </Link>
-                <div className="nav-links-container">
-                    <Link className ="nav-link" to="/shop">
+            <NavigationContainer>
+                <LogoContainer to='/'>
+                    <YLogo className='logo'/>
+                </LogoContainer>
+                <NavLinksContainer>
+                    <NavLink to='/shop'>
                         Shop
-                    </Link>
+                    </NavLink>
                     {
                         currentUser ? (
-                            <span className='nav-link' >Sign Out</span>                             
+                            <NavLink as='span' onClick={signOutUser} >Sign Out</NavLink>                             
                         ):(
-                        <Link className ="nav-link" to="/sign-in">
+                        <NavLink to="/sign-in">
                             Sign In
-                        </Link>
+                        </NavLink>
                         )
                     }
                     <CartIcon/>
-                </div>
+                </NavLinksContainer>
                 {
                     cartDropDown &&
                     <CartDropDown/>
                 }
                               
-            </div>
+            </NavigationContainer>
             <Outlet/>
         </Fragment>
     )
