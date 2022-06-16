78aa1e3aabe6e86feb6debf66236b2c8b7419e58
RonaldVilorio <ronvilorio@gmail.com>
2022-06-16 14:26:49 -0400

    cart-item scss to styled comp

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,14 +1,19 @@
-import './cart-item.styles.scss'
+import {
+    cartItemContainer,
+    ItemDetails,
+    Name,
+    Image} from './cart-item.styles'
+
 const CartItem = ({cartItem}) => {
     const {name,quantity,imageUrl,price} = cartItem   
     return ( 
-        <div className='cart-item-container'>
-            <img src={imageUrl} alt={name} />
-            <div className='item-details'>
-                <span className='name'>{name}</span>
+        <cartItemContainer>
+            <Image src={imageUrl} alt={name} />
+            <ItemDetails>
+                <Name>{name}</Name>
                 <span className='price'>{quantity} X ${price}</span>
-            </div>
-        </div>
+            </ItemDetails>
+        </cartItemContainer>
      );
 }
  
