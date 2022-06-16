92c129356904a9bb235f06bf081f1151929bdb62
RonaldVilorio <ronvilorio@gmail.com>
2022-06-16 14:54:37 -0400

    checkout-item scss to styled comp

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,5 +1,5 @@
 import ProductCard from '../product-card/product-card.component';
-import {CategoryPreviewContainer, Title,Preview} from  './category-preview.styles.scss';
+import {CategoryPreviewContainer, Title,Preview} from  './category-preview.styles';
 
 const CategoryPreview = ({ title, products }) => {
     console.log('Hey')
--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,6 +1,16 @@
-import './checkout-item.styles.scss'
 import { useContext } from 'react';
 import { CartDropDownContext } from '../../contexts/cart-drop-down.context';
+import {
+    CheckoutItemContainer,
+    ImageContainer,
+    Image,
+    Name,
+    Quantity,
+    Arrow,
+    Price,
+    Value,
+    RemoveButton
+}from './checkout-item.styles'
 
 const CheckoutItem = ({cartItem}) => {
     const {name,imageUrl,price,quantity} = cartItem
@@ -11,23 +21,23 @@ const CheckoutItem = ({cartItem}) => {
 
    
         return ( 
-            <div className='checkout-item-container'>
-                <div className='image-container'>
-                    <img src={imageUrl} alt={name} />
-                </div>
-                <span className='name'>{name}</span>
-                <span className='quantity'>
-                    <div className='arrow' onClick={removeItemHandler}>
+            <CheckoutItemContainer>
+                <ImageContainer>
+                    <Image src={imageUrl} alt={name} />
+                </ImageContainer>
+                <Name>{name}</Name>
+                <Quantity>
+                    <Arrow onClick={removeItemHandler}>
                         &#10094;
-                    </div>
-                    <span className='value'>{quantity}</span>
-                    <div className='arrow' onClick={addItemHandler}>
+                    </Arrow>
+                    <Value>{quantity}</Value>
+                    <Arrow onClick={addItemHandler}>
                         &#10095;
-                    </div>
-                </span>
-                <span className='price'>{price}</span>
-                <div className='remove-button' onClick={clearItemHandler}>&#10005;</div>
-            </div>
+                    </Arrow>
+                </Quantity>
+                <Price>{price}</Price>
+                <RemoveButton onClick={clearItemHandler}>&#10005;</RemoveButton>
+            </CheckoutItemContainer>
          );
 }
  
