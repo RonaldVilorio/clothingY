88dd2e657070ef6ac7b4eb7f01fbad2464ed058b
RonaldVilorio <ronvilorio@gmail.com>
2022-06-16 17:36:33 -0400

    checkout route comp scss to styled, delted unneeded shop scss file

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,29 +1,35 @@
 import { useContext } from "react";
 import { CartDropDownContext } from "../../contexts/cart-drop-down.context";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
-import './checkout.styles.scss'
+import {
+    CheckoutContainer,
+    CheckoutHeader,
+    HeaderBlock,
+    Total
+} from './checkout.styles'
+
 const CheckOut = () => {
     const {cartItems,cartTotal} = useContext(CartDropDownContext)
     
     return (
-        <div className='checkout-container'>
-            <div className='checkout-header'>
-                <div className='header-block'>
+        <CheckoutContainer>
+            <CheckoutHeader>
+                <HeaderBlock>
                     <span>Product</span>
-                </div>
-                <div className='header-block'>
+                </HeaderBlock>
+                <HeaderBlock>
                     <span>Description</span>
-                </div>
-                <div className='header-block'>
+                </HeaderBlock>
+                <HeaderBlock>
                     <span>Quantity</span>
-                </div>
-                <div className='header-block'>
+                </HeaderBlock>
+                <HeaderBlock>
                     <span>Price</span>
-                </div>
-                <div className='header-block'>
+                </HeaderBlock>
+                <HeaderBlock>
                     <span>Remove</span>
-                </div>
-            </div>
+                </HeaderBlock>
+            </CheckoutHeader>
             {
                 cartItems.map((cartItem)=>{
                     return(
@@ -31,8 +37,8 @@ const CheckOut = () => {
                     )
                 })
             }
-            <div className='total'>Total: ${cartTotal}</div>
-        </div>
+            <Total>Total: ${cartTotal}</Total>
+        </CheckoutContainer>
     )                    
 }
 export default CheckOut;
